6fac3cc80729722bad17479184b11b63bd27517b
Hao Phan <rinnphan01@gmail.com>
2024-12-28 18:03:55 -0800

    These are working for now

--- hao/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <h1>Hello, React 18!</h1>
-  </React.StrictMode>
-);
