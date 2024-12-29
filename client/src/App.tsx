e5ca8e8611fbbb49017c5d05a282935d6234a849
Hao Phan <rinnphan01@gmail.com>
2024-12-28 19:04:26 -0800

    add some stuff to use

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route,  Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
 
 const App: React.FC = () => {
-    return <div>Hello, React!</div>;
+    return (
+      <Router>
+          <nav style={styles.nav}>
+              <Link to="/Home" style={styles.link}>Home</Link>
+              <Link to="/About" style={styles.link}>About</Link>
+          </nav>
+          <Routes>
+              <Route path="/Home" element={<Home />} />
+              <Route path="/About" element={<About />} />
+          </Routes>
+      </Router>
+  );
+};
+
+const styles = {
+  nav: {
+      display: 'flex',
+      justifyContent: 'center',
+      padding: '10px',
+      backgroundColor: '#007BFF',
+  },
+  link: {
+      margin: '0 15px',
+      color: 'white',
+      textDecoration: 'none',
+      fontSize: '1.2rem',
+  },
 };
 
 export default App;
+
---
+++ client/src/pages/About.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return <div>Hello, This will be about my about page</div>;
+};
+
+export default About;
