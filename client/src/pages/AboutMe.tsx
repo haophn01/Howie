d496a34fc6411e1e04827f64a1f04556bcc8beec
Hao Phan <rinnphan01@gmail.com>
2024-12-31 18:47:27 -0800

    Add basic button

--- client/src/pages/AboutMe.tsx
+++ client/src/pages/AboutMe.tsx
@@ -1,7 +1,9 @@
 import {Typography} from '@mui/material';
+import BackButton from '../Components/BackButton';
 export default function AboutMe() {
     return (
         <div>
+            <BackButton/>
             <Typography variant="body1">
                 This is my about me page
             </Typography>
--- client/src/pages/Project.tsx
+++ client/src/pages/Project.tsx
@@ -1,7 +1,9 @@
 import { Typography } from '@mui/material';
+import BackButton from '../Components/BackButton';
 export default function Project() {
     return (
         <div>
+            <BackButton/>
             <Typography variant="body1"> 
                 Welcome to my project page 
             </Typography>
