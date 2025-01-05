ed9ca741453692b36a085ca959ad9d677c417284
Hao Phan <rinnphan01@gmail.com>
2025-01-05 14:53:34 -0800

    Able to add the book in

--- client/src/pages/Project.tsx
+++ client/src/pages/Project.tsx
@@ -1,43 +1,79 @@
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import HTMLFlipBook from 'react-pageflip';
 import BackButton from '../Components/BackButton';
 export default function Project() {
     return (
-        <div>
-            <BackButton name="About Me" />
-            <Typography variant="body1">
-                Welcome to my project page
-            </Typography>
-            <HTMLFlipBook 
-                width={300} 
+        <Box style={{
+            backgroundColor: '#F7F4DF',
+            height: '100vh',
+            display: "flex",
+            flexDirection: 'row',
+            alignItems: 'center',
+        }}>
+            {/* Left Box */}
+            <Box style={{
+                borderColor: "black",
+                width: '30%',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+
+                <Typography variant="h4"><BackButton name="" />Project</Typography>
+            </Box>
+
+            {/* Middle Box */}
+            <Box style={{
+                width: '4px',
+                backgroundColor: '#D9D9D9',
+                height: "60%",
+                margin: '0 1rem',
+                borderRadius: '10px',
+                border: "solid",
+            }}></Box>
+
+            {/* Right Box */}
+            <Box style={{
+                borderColor: "black",
+                width: '70%',
+                justifyContent: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+            }}>
+                <HTMLFlipBook 
+                width={600} 
                 height={500} 
-                className="" 
-                style={{}} 
-                maxWidth={300} 
-                minWidth={300} 
-                maxHeight={500} 
+                className=""  
+                maxWidth={1000} 
+                minWidth={700} 
+                maxHeight={1000} 
                 minHeight={500}
                 size="fixed"
                 startPage={0}
                 drawShadow={true}
                 flippingTime={1000}
                 useMouseEvents={true}
-                swipeDistance={30}
-                showCover={true}
-                mobileScrollSupport={true}
+                swipeDistance={50}
+                showCover={false}
+                mobileScrollSupport={false}
                 clickEventForward={true}
                 usePortrait={true}
                 startZIndex={0}
                 autoSize={true}
                 maxShadowOpacity={0.5}
                 showPageCorners={true}
-                disableFlipByClick={false}
+                disableFlipByClick={true}
+                style={{
+                    background: 'white',
+                    boxShadow: '0 0 20px rgba(0,0,0,0.3)',
+                }}
             >
                 <div className="demoPage">Page 1</div>
                 <div className="demoPage">Page 2</div>
                 <div className="demoPage">Page 3</div>
                 <div className="demoPage">Page 4</div>
             </HTMLFlipBook>
-        </div>
+                
+            </Box>
+        </Box>
     )
 }
